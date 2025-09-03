2cfc3af1279f2995f913c0500abfaebd28a3251d
Joel Izzedin <joelizzedin.dev@gmail.com>
2025-09-03 11:28:58 -0400

    set up database

--- server.js
+++ server.js
@@ -5,23 +5,26 @@
 const express = require("express")
 const app = express()
 
-
-
 // Importing .env
 const dotenv = require("dotenv") // can also write it as require("dotenv").config()
 dotenv.config()
 const PORT = process.env.PORT // pulling PORT from .env file
-console.log(process.env)
+// console.log(process.env) // just to test
 
-// Importing Mongoose
+// Importing Mongoose - Database Setup
 const mongoose = require("mongoose")
 mongoose.connect(process.env.MONGODB_URI) // connecting to MongoDB database
+
+const db = mongoose.connection
+db.on("error", (err) => {console.log("ERROR: ", err)}) // connection message that will print to our terminal when there is an error
+db.on("connected", () => {console.log(`Conntected to MongoDB ${mongoose.connection.name}.`)}) // connection message that will print to our terminal when we’ve connected to the database
+db.on("disconnected", () => {console.log("mongo disconnected")}) // connection message that will print to our terminal when we’ve disconnected
 
 
 /* --- ROUTES --- */
 
-app.get("/", async (req, res) => {
-    res.send("hello, friend!")
+app.get("/", async (req, res) => { // rendering the index.ejs page content in the browser
+    res.render("index.ejs")
 })
 
 /* --- LISTENER --- */
