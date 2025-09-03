f160e9adb891f6500777fb92c46a4615b84b2388
Joel Izzedin <joelizzedin.dev@gmail.com>
2025-09-03 11:09:33 -0400

    setup dotenv

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 
 /* --- IMPORTED MODULES --- */
 
+// Importing Express
 const express = require("express")
 const app = express()
-const PORT = 3000 // defining PORT as a variable, best practice 
+
+
+
+// Importing .env
+const dotenv = require("dotenv") // can also write it as require("dotenv").config()
+dotenv.config()
+const PORT = process.env.PORT // pulling PORT from .env file
+console.log(process.env)
+
+// Importing Mongoose
+const mongoose = require("mongoose")
+mongoose.connect(process.env.MONGODB_URI) // connecting to MongoDB database
+
 
 /* --- ROUTES --- */
+
 app.get("/", async (req, res) => {
     res.send("hello, friend!")
 })
 
+/* --- LISTENER --- */
 
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT: ${PORT}`) // see imported modules
