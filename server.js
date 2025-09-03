3e32f28ff26b8ab67d52d19445d7943e948a9efc
Joel Izzedin <joelizzedin.dev@gmail.com>
2025-09-03 15:00:08 -0400

    added method-override and morgan middleware

--- server.js
+++ server.js
@@ -2,16 +2,19 @@
 /* ------------ IMPORTING MODULES ------------ */
 
 // Importing Express
+
 const express = require("express")
 const app = express()
 
 // Importing .env
+
 const dotenv = require("dotenv") // can also write it as require("dotenv").config()
 dotenv.config()
 const PORT = process.env.PORT // pulling PORT from .env file
 // console.log(process.env) // just to test
 
 // Importing Mongoose - Database Setup
+
 const mongoose = require("mongoose")
 mongoose.connect(process.env.MONGODB_URI) // connecting to MongoDB database
 
@@ -20,14 +23,25 @@ db.on("error", (err) => {console.log("ERROR: ", err)}) // connection message tha
 db.on("connected", () => {console.log(`Conntected to MongoDB ${mongoose.connection.name}.`)}) // connection message that will print to our terminal when we’ve connected to the database
 db.on("disconnected", () => {console.log("mongo disconnected")}) // connection message that will print to our terminal when we’ve disconnected
 
+// Importing Method-Override
+
+const methodOverride = require("method-override")
+    // Allows us to set up PUT and DELETE routes through a form
+    // GET and POST are built into forms, but not PUT and DELETE
+
+// Importing Morgan Middlewear
+
+const morgan = require("morgan") 
+
 // Importing Fruits Model
 
 const Fruit = require("./models/fruit.js")
 
-// Importing Middleware
+// Using Middleware
     
 app.use(express.urlencoded({ extended: true })) // "express.urlencoded" middleware parses incoming request bodies, extracting form data and converting it into a JavaScript object.
-
+app.use(methodOverride("_method"))
+app.use(morgan("dev"))
 
 /* ------------ ROUTES ------------ */
 
